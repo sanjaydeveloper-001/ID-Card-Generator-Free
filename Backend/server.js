e963e1a9eb7af38d44c20a1bdf540fe9f88f75f6
Sanjay <josephstudent001@gmail.com>
2026-04-01 09:34:48 +0530

    Updated server

--- Backend/server.js
+++ Backend/server.js
@@ -17,10 +17,7 @@ app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URL || process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch(err => console.error("❌ MongoDB Error:", err));
 
