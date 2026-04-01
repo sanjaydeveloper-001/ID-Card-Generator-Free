c3f79a0f47400df83fe62338a4d3c63d38d5f753
Sanjay <josephstudent001@gmail.com>
2026-04-01 09:47:07 +0530

    Updated to host properly

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const fetch = require('node-fetch');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const userRoutes = require('./routes/userRoutes');
@@ -14,7 +13,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true, limit: "20mb" }));
-app.use(bodyParser.json());
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URL)
@@ -22,6 +20,10 @@ mongoose.connect(process.env.MONGO_URL)
   .catch(err => console.error("❌ MongoDB Error:", err));
 
 // Routes
+app.get('/', (req, res) => {
+  res.send("🚀 Server is running");
+});
+
 app.use("/api/user", userRoutes);
 
 // Email validation using Abstract API
